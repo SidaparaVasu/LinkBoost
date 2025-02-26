b72eb65f30e2c3828fc7b6d1f5f123086a7ac0e7
SidaparaVasu <vasupatel303@gmail.com>
2025-02-26 13:42:42 +0530

    Update Accounts/models.py
    • Add Referral model

--- LinkBoost/Accounts/models.py
+++ LinkBoost/Accounts/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
@@ -13,3 +16,13 @@ class CustomUser(AbstractUser):
         if not self.referral_code:
             self.referral_code = str(uuid.uuid4().hex[:20])  # Generate unique referral code
         super().save(*args, **kwargs)
+
+
+class Referral(models.Model):
+    referrer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="referrer_referrals")
+    referred_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="referred_by_referral")
+    date_referred = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[("pending", "Pending"), ("successful", "Successful")], default="pending")
+
+    def __str__(self):
+        return f"{self.referrer.username} → {self.referred_user.username} ({self.status})"
