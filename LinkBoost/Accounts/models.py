2046c119d327a341f8d04ace7e5631f043cad208
SidaparaVasu <vasupatel303@gmail.com>
2025-02-26 13:49:43 +0530

    Update Accounts/models.py
    • Modified Referral model

--- LinkBoost/Accounts/models.py
+++ LinkBoost/Accounts/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 
-User = get_user_model()
+# User = get_user_model()
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
@@ -19,8 +19,8 @@ class CustomUser(AbstractUser):
 
 
 class Referral(models.Model):
-    referrer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="referrer_referrals")
-    referred_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="referred_by_referral")
+    referrer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="referrer_referrals")
+    referred_user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="referred_by_referral")
     date_referred = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=[("pending", "Pending"), ("successful", "Successful")], default="pending")
 
